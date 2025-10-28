e7aa0bac7a28a8402b6180bcd6ff8e29d34e3306
mumeriftikharpk <epicumergaming@gmail.com>
2025-10-28 22:34:05 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,17 +2,13 @@ const CACHE_NAME = "cartel-calc-v1";
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./manifest.json",
-  "./main.js"
+  "./manifest.json"
 ];
 
 self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
 });
+
 self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
